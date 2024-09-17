7c0914da3b67a0c9df38e82648d7879b39ce9cbe
Vanna Panhavorn <108249074+vornone@users.noreply.github.com>
2024-09-17 11:28:56 +0700

    updated

--- src/renderer/src/components/ProductContainer.jsx
+++ src/renderer/src/components/ProductContainer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Box } from '@chakra-ui/react'
 function ProductContainer({ children }) {
   return (
-    <Box width="100%" height="150px" borderRadius={10} overflow={'hidden'} shadow={'md'}>
+    <Box width="100%" height="200px" borderRadius={10} overflow={'hidden'} shadow={'md'}>
       {children}
     </Box>
   )
