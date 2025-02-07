41d5b29b5ed29f973c87c5dbcdf9ea59a91b674e
vornone <108249074+vornone@users.noreply.github.com>
2025-02-07 22:52:45 +0700

    update

--- src/renderer/components/admin/AdminPage.jsx
+++ src/renderer/components/admin/AdminPage.jsx
@@ -1,44 +1,53 @@
-import { Button, Fieldset, Input, Stack, Text} from "@chakra-ui/react"
-import { Field } from "@/components/ui/field"
-import {Card } from "@chakra-ui/react"
-import { PasswordInput } from '@/components/ui/password-input';
-import { ColorModeButton } from "@/components/ui/color-mode"
-import { NumberInputField, NumberInputRoot } from "@/components/ui/number-input"
-import { InputGroup } from "@/components/ui/input-group"
-import React,{ useState} from "react";
- const AdminPage = () => {
+import { Button, Fieldset, Input, Stack, Text } from '@chakra-ui/react'
+import { Field } from '@/components/ui/field'
+import { Card } from '@chakra-ui/react'
+import { PasswordInput } from '@/components/ui/password-input'
+import { ColorModeButton } from '@/components/ui/color-mode'
+import { NumberInputField, NumberInputRoot } from '@/components/ui/number-input'
+import { InputGroup } from '@/components/ui/input-group'
+import React, { useState } from 'react'
+const AdminPage = () => {
   const [isEditable, setIsEditable] = useState(false)
   return (
-    <Card.Root p={10}  borderRadius={20}>
-    <Fieldset.Root size="lg" maxW="md"  >
-      <Stack>
-        <Fieldset.Legend fontSize="2xl">Admin Page</Fieldset.Legend>
-        <Fieldset.HelperText>
-          Edit your essential data
-        </Fieldset.HelperText>
-      </Stack>
-      <Fieldset.Content>
-        <Field label="Minimum Stock" helperText="Show alert when stock is less or equal to the amount" >
-          <Input name="name" defaultValue={10} disabled={!isEditable}/>
-        </Field>
-        <Field label="Khmer Riel" helperText="Amount equal to 1USD"     >
-          <NumberInputRoot
-            w="full"
-            formatOptions={{
-              style: "currency",
-              currency: "KHR",
-            }}
-            step={10}
-            defaultValue={299}
+    <Card.Root p={10} borderRadius={20} shadow={'md'}>
+      <Fieldset.Root size="lg" maxW="md">
+        <Stack>
+          <Fieldset.Legend fontSize="2xl">Admin Page</Fieldset.Legend>
+          <Fieldset.HelperText>Edit your essential data</Fieldset.HelperText>
+        </Stack>
+        <Fieldset.Content>
+          <Field
+            label="Minimum Stock"
+            helperText="Show alert when stock is less or equal to the amount"
           >
-            <NumberInputField  disabled={!isEditable}		/>
-          </NumberInputRoot>
-        </Field>
-      </Fieldset.Content>
-      <Button type="submit" alignSelf="flex-end" colorPalette={'blue'} onClick={()=>setIsEditable(!isEditable)} variant={isEditable ? "solid" : "outline"}>
-        {isEditable ? "Save" : "Edit"}
-      </Button>
-    </Fieldset.Root>
+            <Input name="name" defaultValue={10} disabled={!isEditable} />
+          </Field>
+          <Field label="Khmer Riel" helperText="Amount equal to 1USD">
+            <NumberInputRoot
+              w="full"
+              formatOptions={{
+                style: 'currency',
+                currency: 'KHR'
+              }}
+              step={10}
+              disabled={!isEditable}
+              defaultValue={299}
+            >
+              <NumberInputField />
+            </NumberInputRoot>
+          </Field>
+        </Fieldset.Content>
+        <Button
+          type="submit"
+          alignSelf="flex-end"
+          colorPalette={'blue'}
+          onClick={() => setIsEditable(!isEditable)}
+          variant={isEditable ? 'solid' : 'outline'}
+        >
+          {isEditable ? 'Save' : 'Edit'}
+        </Button>
+        <ColorModeButton />
+      </Fieldset.Root>
     </Card.Root>
   )
 }
